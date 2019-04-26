c2db05c25f7974ab9a5b3c28b93bd26d68f2de65
Nickbarbieri1 <nicolo.barbieri@fermi.mo.it>
2019-04-26 10:12:33 +0200

    Editing Main

--- Main.py
+++ Main.py
@@ -2,3 +2,10 @@ import Bot
 import MQTT
 import Thinkspeak
 import threading
+
+a = threading.Thread(target=Bot.main)
+b = threading.Thread(target=MQTT.main)
+c = threading.Thread(target=Thinkspeak.main)
+b.start()
+c.start()
+a.start()
